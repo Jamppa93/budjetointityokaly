d223e044dada507984874d284f05d5552a5b2862
Jamppa <jan.saariniemi@gmail.com>
2020-05-30 12:01:14 +0300

    mainClassDraft

--- python/main.py
+++ python/main.py
@@ -1,1 +1,36 @@
-print('hello world')
+######################################################################
+# title: Budjetointityokalu
+# authors: Jan Saariniemi & Anssi Lillomäki & Santeri Lipsunen
+# date: 30.5.2020
+######################################################################
+#libraries
+import time
+######################################################################
+#tic
+t = time.time()
+
+class mainClass(): 
+    checkNfile = 0
+    tempfileName = ''
+    fileLocDirectories = []
+    
+    def __init__(self):
+        self.checkNfile = 0
+        self.tempfileName = ''
+        self.fileLocDirectories = ['budjetointityokalu/newFiles','budjetointityokalu/usedFiles','budjetointityokalu/dbFile']  
+    
+    def getFileLocDirectories(self):
+        return(self.fileLocDirectories)
+                           
+              
+######################################################################
+main = mainClass()
+print(main.getFileLocDirectories())
+
+
+
+#toc
+print((time.time() - t))
+
+
+
