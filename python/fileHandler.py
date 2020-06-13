ff7503202c9fef4ee062d1a6e20647add7e538ff
Jamppa <jan.saariniemi@gmail.com>
2020-06-13 14:51:20 +0300

    PDFReaderImplemenationPhase

--- python/fileHandler.py
+++ python/fileHandler.py
@@ -1,5 +1,22 @@
+
+import PyPDF2
+#https://medium.com/@umerfarooq_26378/python-for-pdf-ef0fac2808b0
+
 class fileHandler(): 
+
 
     def printObjectType(self):
         print(type(self))
   
+
+    def readPdf(self,filePath):
+        
+        pdfFileObj = open(filePath, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+        for i in range(pdfReader.numPages):
+            pageObj = pdfReader.getPage(i)
+            print(pageObj.extractText())
+        
+        
+
+
--- python/main.py
+++ python/main.py
@@ -6,42 +6,106 @@
 #libraries
 import time
 import sys
+import pathlib # https://docs.python.org/3/library/pathlib.html#module-pathlib
 from etlManager import etlManager
 from fileHandler import fileHandler
 ######################################################################
-#tic
-t = time.time()
-
+#MainClass
 class mainClass(): 
-    checkNfile = 0
+    checkNfile = [] # muutetaan listaksi
     tempfileName = ''
     fileLocDirectories = []
     etlManagerInst = None
     fileHandlerInst = None
     
     def __init__(self):
-        self.checkNfile = 0
+        self.checkNfile = []
         self.tempfileName = ''
-        self.fileLocDirectories = ['budjetointityokalu/newFiles','budjetointityokalu/usedFiles','budjetointityokalu/dbFile']  
+        self.fileLocDirectories = ['newFiles','usedFiles','dbFile']  
         self.etlManagerInst = etlManager()
         self.fileHandlerInst = fileHandler()
+        
+    def getRecentCheckNfile():
+        pass
     
-    def getFileLocDirectories(self):
-        return(self.fileLocDirectories)
-                           
+    def getNewFilesPath(self):
+        return self.fileLocDirectories[0]
+    
+    def getoldFilesPath(self):
+        return self.fileLocDirectories[1]
+    
+    def getdbPath(self):
+        return self.fileLocDirectories[2]
+    
+    def isFileStructureOk(self):
+        fileStructureIsOk = True
+        tempList = []
+        
+        for i in self.fileLocDirectories:
+            tempList.append(pathlib.Path(pathlib.Path(__file__).parents[1].absolute(), i))
+
+        for i in tempList:
+            if i.is_dir() == False: #path doesn’t exist, is a broken symlink  or it is not a regular file
+                fileStructureIsOk = False
+                break 
+            
+        if  fileStructureIsOk == True:
+            self.fileLocDirectories = tempList  #replace the filedirectory paths 
+        return fileStructureIsOk
+
+
+
+
+    def getRecentNewFileNamesToList(self):
+        
+        path = pathlib.Path(self.getNewFilesPath())
+        tempList = []
+        for fileDir in path.rglob('*.pdf'):
+            tempList.append(fileDir)
+        #Should do somehting to files which are not pdf. tyyliin clear all tai jotain mut ois tehtävä vasta lopussa
+        return tempList
+
+
+
+
+    def startProcess(self):
+        
+        
+        
+        #print(self.getNewFilesPath())
+        self.checkNfile = self.getRecentNewFileNamesToList()
+        #print(self.checkNfile)
+        fileCount = len(self.checkNfile)
+        while (fileCount>0):
+            print(self.checkNfile[fileCount-1])
+            tempFile = self.fileHandlerInst.readPdf(self.checkNfile[fileCount-1])
+            ## main.etlManagerInst.etlFile(tempFile)
+            fileCount -=1 
+
+        ##clearAllFromNewFilesFolder() tai joku tämmönen
+
+  
+######################################################################
+#MainProgram 
+
+def mainProg():
+    main = mainClass()
+    isItGo =main.isFileStructureOk()
+    if (isItGo == False):
+        print("Programs's file structure is not correct: some program files (such as 'newFiles','usedFiles','dbFile) are missing.") #error1 for ErrorObj
+    else:
+        main.startProcess()
     
 ######################################################################
-main = mainClass()
-
-
-
-print(main.getFileLocDirectories())
-main.fileHandlerInst.printObjectType()
-main.etlManagerInst.printObjectType()
-
-
+#EXECUTE
+#tic
+t = time.time()
+###################################
+mainProg()
+###################################
 #toc
 print((time.time() - t))
+######################################################################
 
 
 
